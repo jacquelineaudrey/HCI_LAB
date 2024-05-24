b28a7ea9f6ef994f7148f796adb60324739837af
jacquelineaudrey <jacquelineaudreyiman@gmail.com>
2024-05-24 17:03:38 +0700

    fix the features carousel

--- home.js
+++ home.js
@@ -1,12 +1,64 @@
+/* ============================= UTILS ============================== */
+function vw(percent) {
+    const w = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+    return (percent * w) / 100;
+  }
+
+
+
 /* ============================= FEATURES ============================== */
 
 const container = document.querySelector('.inner');
 const slides = document.querySelectorAll('.slide');
 const prevButton = document.getElementById('prev');
 const nextButton = document.getElementById('next');
-const slideWidth = slides[0].offsetWidth;
-const visibleSlides = 4; 
+let visibleSlides = 4; 
 let currentIndex = 0;
+
+// BASE ELEMENT
+
+const sliderPadding = vw(6);
+
+const sliderContainer = document.querySelector("#slider");
+const slideList = Array.from(sliderContainer.querySelectorAll(".slide"));
+let elementWidth = 0;
+
+const baseElement = slideList[0].cloneNode(true);
+baseElement.style.visibility = "hidden";
+sliderContainer.insertAdjacentElement("afterbegin", baseElement);
+
+function resizeSlider() {
+    const containerWidth = sliderContainer.getBoundingClientRect().width; 
+    elementWidth = containerWidth / visibleSlides - vw(3.5);
+    
+    for(let i = 0; i < slideList.length; i++) {
+        slideList[i].style.width = `${elementWidth}px`
+    }
+}
+
+function matchViewportWidth() {
+    if(window.innerWidth < 768) {
+        visibleSlides = 2;
+        // console.log("MINI TABLET")
+
+    }else if(window.innerWidth < 1200) {
+        visibleSlides = 3;
+        // console.log("TABLET")
+
+    } else {
+        visibleSlides = 4;
+        // console.log("DESKTOP");
+
+    }
+}
+
+window.addEventListener("resize", () => {
+    matchViewportWidth();
+    resizeSlider();
+    updateSlider();
+})
+
+resizeSlider();
 
 prevButton.addEventListener('click', function () {
     if (currentIndex > 0) {
@@ -23,7 +75,17 @@ nextButton.addEventListener('click', function () {
 });
 
 function updateSlider() {
-    container.style.transform = `translateX(-${slideWidth * currentIndex}px)`;
+    let translateValue = 0;
+    
+    if(currentIndex === 0) {
+        translateValue = (elementWidth * currentIndex - vw(6)) * -1
+    } else if(currentIndex === slideList.length - visibleSlides) {
+        translateValue = ((elementWidth + vw(5))  * currentIndex) * -1;
+    } else {
+        translateValue = ((elementWidth + vw(3.5)) * currentIndex) * -1;
+    }
+    
+    container.style.transform = `translateX(${translateValue}px)`;
 }
 
 updateSlider();

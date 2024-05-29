10365919f141ac0180102fbdacbca9c0361e8f87
jacquelineaudrey <jacquelineaudreyiman@gmail.com>
2024-05-29 12:39:25 +0700

    button learn page color

--- js/learn.js
+++ js/learn.js
@@ -24,4 +24,35 @@ document.addEventListener('DOMContentLoaded', function() {
     // Optionally, you can trigger a click on one of the buttons to set a default view
     document.getElementById('elementary-btn').click();
   });
+
+  
+  document.addEventListener('DOMContentLoaded', (event) => {
+    const heartIcons = document.querySelectorAll('.heart-icon');
+    heartIcons.forEach(icon => {
+      icon.addEventListener('click', () => {
+        icon.classList.toggle('red');
+      });
+    });
+  
+    const errorTriggerButtons = document.querySelectorAll('.play-button.error-trigger');
+    const modal = document.getElementById('errorModal');
+    const closeModal = document.querySelector('.close');
+  
+    errorTriggerButtons.forEach(button => {
+      button.addEventListener('click', (event) => {
+        event.preventDefault();
+        modal.style.display = 'block';
+      });
+    });
+  
+    closeModal.addEventListener('click', () => {
+      modal.style.display = 'none';
+    });
+  
+    window.addEventListener('click', (event) => {
+      if (event.target === modal) {
+        modal.style.display = 'none';
+      }
+    });
+  });
   
